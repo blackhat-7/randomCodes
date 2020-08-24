d437c7eabe02cc039b01dd616f387fc267e3e1a7
blackhat-7 <palshaunak7@gmail.com>
2020-08-24 22:45:49 +0400

    Update

--- .ccls-cache/@home@illusion@Documents@randomCodes/gnu_x86@hw2q2.c
+++
@@ -1,8 +0,0 @@
-int bar(int *x, int *y) {
-    *x = 0;
-    *y = 1;
-}
-
-void foo (int a, int b) {
-    bar(&a, &b);
-}
--- .ccls-cache/@home@illusion@Documents@randomCodes/gnu_x86@hw2q3.c
+++
@@ -1,5 +0,0 @@
-int bar(int x, int y);
-
-int foo (int a, int b) {
-    return bar(a, b);
-}
